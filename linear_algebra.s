39696f9093c917383f1e0d86588f557f34ee812c
MazenAmria <t.mazenamria@gmail.com>
2021-03-20 18:25:37 +0200

    matrix operations... 3/4

--- linear_algebra.s
+++ linear_algebra.s
@@ -5,6 +5,8 @@ allocate_matrix:
   # Parameters:
   # $a0 = 1st dimension (number of rows)
   # $a1 = 2nd dimension (number of cols)
+  # Return values:
+  # $v0 = address of the allocated matrix
   
   # matrix representaion in memory:
   # 1st word: 1st dimension
@@ -43,6 +45,8 @@ allocate_vector:
   # allocates a vector in the memory
   # Parameters:
   # $a0 = size of the vector
+  # Return values:
+  # $v0 = address of the allocated vector
   
   # vector representaion in memory:
   # a single column matrix
@@ -217,4 +221,178 @@ matrix_mult:
   # $a1 = the address of the right matrix (vector)
   # $a2 = the address of the result matrix (vector)
 
-  # TODO: implement :)
+  # check matrices dimensions:
+  # left 1st = result 1st
+  lw $t0, 0($a0)
+  lw $t3, 0($a2)
+  bne $t0, $t3, value_error
+  # left 2nd = right 1st
+  lw $t1, 4($a0)
+  lw $t4, 0($a1)
+  bne $t1, $t4, value_error
+  # right 2nd = result 2nd
+  lw $t2, 4($a1)
+  lw $t5, 4($a2)
+  beq $t2, $t5, evaluate
+
+  value_error:
+  li $v1, -1 # should be replaced with constant
+  jal exit
+
+  evaluate:
+  # define pointers to the matrices
+  addiu $t3, $a0, 8 # left matrix' pointer
+  addiu $t4, $a1, 8 # right matrix' pointer
+  addiu $t5, $a2, 8 # result matrix' pointer
+
+  # define step size for the right matrix
+  li $t6, 4
+  multu $t2, $t6
+  mflo $t6
+
+  # define step size for the left matrix
+  li $t9, 4
+  multu $t1, $t9
+  mflo $t9
+
+  left_rows:
+    # check if no more rows to iterate
+    beq $t0, $zero, end_rows
+    
+    # save the columns counter to use it in the next iteration
+    addiu $sp, $sp, -4
+    sw $t2, 0($sp)
+
+    # return to the first column
+    move $t8, $t4
+
+    right_cols:
+      # check if no more columns to iterate
+      beq $t2, $zero, end_cols
+
+      # return to the beginning of the row
+      move $t7, $t3
+
+      # save the entries counter to use it in the next iteration
+      addiu $sp, $sp, -8
+      sw $t1, 4($sp)
+      # save the beginning of the column
+      sw $t8, 0($sp)
+
+      # initialize the result by zero
+      mtc1 $zero, $f2
+
+      entries:
+        # check if no more entries to iterate
+        beq $t1, $zero, end_entries
+
+        # multiply the entries and add to the result
+        l.s $f0, 0($t7)
+        l.s $f1, 0($t8)
+
+        mul.s $f3, $f0, $f1
+        add.s $f2, $f2, $f3
+        
+        # move to the next entries
+        addiu $t7, $t7, 4
+        addu $t8, $t8, $t6
+
+        # decrement the entries counter 
+        addiu $t1, $t1, -1
+
+        # loop
+        j entries
+      end_entries:
+
+      # save the result
+      s.s $f2, 0($t5)
+      
+      # move to the next entry in the result
+      addiu $t5, $t5, 4
+
+      # retrieve the entries counter
+      lw $t1, 4($sp)
+      # retrieve the beginning of the column
+      lw $t8, 0($sp)
+      addiu $sp, $sp, 8
+
+      # move to the next column
+      addiu $t8, $t8, 4
+
+      # decrement the columns counter 
+      addiu $t2, $t2, -1
+
+      # loop
+      j right_cols
+    end_cols:
+
+    # move to the next row
+    addu $t3, $t3, $t9
+    
+    # retrieve the columns counter
+    lw $t2, 0($sp)
+    addiu $sp, $sp, 4
+
+    # decrement the rows counter
+    addiu $t0, $t0, -1
+
+    # loop
+    j left_rows
+  end_rows:
+
+  jr $ra
+
+.globl print_matrix
+
+print_matrix:
+  # Prints a matrix (or vector) on screen
+  # Parameters:
+  # $a0 = the address of the matrix (vector)
+
+  lw $t0, 0($a0)
+  lw $t1, 4($a0)
+
+  addiu $t3, $a0, 8
+
+  print_row:
+    # check if no more rows to print
+    beq $t0, $zero, end_rows
+    
+    # get the count of entries in each row
+    move $t2, $t1
+    
+    print_col:
+      # check if no more entries to print
+      beq $t2, $zero, p_end_col
+
+      # print the entry
+      l.s $f12, 0($t3)
+      li $v0, 2
+      syscall
+
+      # print \t
+      li $a0, 9
+      li $v0, 11
+      syscall
+
+      # move to next entry
+      addiu $t3, $t3, 4
+
+      # decrement the count of entries
+      addiu $t2, $t2, -1
+
+      # loop
+      j print_col
+    p_end_col:
+
+    # print new line
+    li $a0, 10
+    li $v0, 11
+    syscall
+
+    # decrement the count of rows
+    addiu $t0, $t0, -1
+
+    # loop
+    j print_row
+  p_end_rows:
--- main.s
+++ main.s
@@ -6,19 +6,95 @@ main:
   li $a1, 5
   jal allocate_matrix
 
+  addiu $sp, $sp, -4
+  sw $v0, 0($sp)
+
   move $a0, $v0
   li $a1, 5
+  mtc1 $a1, $f2
+  cvt.s.w $f2, $f2
+  mfc1 $a1, $f2
   jal fill
 
   move $a0, $v0
   li $a1, 1
   li $a2, 4
+  mtc1 $a2, $f2
+  cvt.s.w $f2, $f2
+  mfc1 $a2, $f2
   jal fill_row
 
   move $a0, $v0
   li $a1, 3
   li $a2, 3
+  mtc1 $a2, $f2
+  cvt.s.w $f2, $f2
+  mfc1 $a2, $f2
   jal fill_col
+
+  move $a0, $v0
+  jal print_matrix
+
+  # print new line
+  li $a0, 10
+  li $v0, 11
+  syscall
+
+  li $a0, 5
+  li $a1, 4
+  jal allocate_matrix
+
+  addiu $sp, $sp, -4
+  sw $v0, 0($sp)
+
+  move $a0, $v0
+  li $a1, 5
+  mtc1 $a1, $f2
+  cvt.s.w $f2, $f2
+  mfc1 $a1, $f2
+  jal fill
+
+  move $a0, $v0
+  li $a1, 1
+  li $a2, 4
+  mtc1 $a2, $f2
+  cvt.s.w $f2, $f2
+  mfc1 $a2, $f2
+  jal fill_col
+
+  move $a0, $v0
+  li $a1, 3
+  li $a2, 3
+  mtc1 $a2, $f2
+  cvt.s.w $f2, $f2
+  mfc1 $a2, $f2
+  jal fill_row
+
+  move $a0, $v0
+  jal print_matrix
+
+
+  # print new line
+  li $a0, 10
+  li $v0, 11
+  syscall
+
+  li $a0, 4
+  li $a1, 4
+  jal allocate_matrix
+
+  move $a2, $v0
+
+  lw $a1, 0($sp)
+  lw $a0, 4($sp)
+  addiu $sp, $sp, 4
+
+  sw $a2, 0($sp)
+  jal matrix_mult
+
+  lw $a0, 0($sp)
+  addiu $sp, $sp, 4
+  jal print_matrix
 
   li $v1, 0
   jal exit
