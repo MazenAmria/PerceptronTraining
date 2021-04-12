e36ccba9c69dbf42d5a8d56208d90a7061fa689d
MazenAmria <t.mazenamria@gmail.com>
2021-04-12 12:05:48 +0300

    Organizing files

--- testcases/generate_linalg_debug_modules_test.sh
+++ testcases/generate_linalg_debug_modules_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat linalg_debug_modules_test.s > main.s
-cat linalg_module/* debug_module/* > temp
+cat ../linalg_module/* ../debug_module/* > temp
 sed -i "/^.*globl.*$/d" temp
 cat temp >> main.s
 rm temp
--- testcases/generate_perceptron_module_test.sh
+++ testcases/generate_perceptron_module_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat perceptron_module_test.s > main.s
-cat *_module/* > temp
+cat ../*_module/* > temp
 sed -i "/^.*globl.*$/d" temp
 cat temp >> main.s
 rm temp
